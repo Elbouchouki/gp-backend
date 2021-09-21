a2a43c4d8634ed18e43ca040481e88685cb4a302
Elbouchouki <elbouchoukigamer@gmail.com>
2021-09-21 21:03:17 +0100

    fixing excel export timeout

--- controllers/excel.controller.js
+++ controllers/excel.controller.js
@@ -7,19 +7,15 @@ const moment = require("moment");
 module.exports = {
   async index(req, res) {
     const ville_id = req.body.ville_id;
-    const date_from = req.body.date_from;
-    const date_to = req.body.date_to;
+    const dates = req.body.dates;
+    console.log(dates);
     try {
-      const resp = await sequelize.query(
-        "call getExcel(:ville_id,:date_f,:date_t)",
-        {
-          replacements: {
-            ville_id: ville_id,
-            date_f: date_from,
-            date_t: date_to,
-          },
-        }
-      );
+      const resp = await sequelize.query("call getNewExcel(:ville_id,:dates)", {
+        replacements: {
+          ville_id: ville_id,
+          dates: dates,
+        },
+      });
 
       res.status(200).json({
         result: resp,
@@ -31,16 +27,13 @@ module.exports = {
     }
   },
   async all(req, res) {
-    const date_from = req.body.date_from;
-    const date_to = req.body.date_to;
+    const dates = req.body.dates;
     try {
-      const resp = await sequelize.query("call getExcel_all(:date_f,:date_t)", {
+      const resp = await sequelize.query("call getNewExcel_all(:dates)", {
         replacements: {
-          date_f: date_from,
-          date_t: date_to,
+          dates: dates,
         },
       });
-
       res.status(200).json({
         result: resp,
       });
@@ -63,6 +56,26 @@ module.exports = {
           },
         }
       );
+
+      res.status(200).json({
+        result: resp,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "error",
+      });
+    }
+  },
+  async dates(req, res) {
+    const date_from = req.body.date_from;
+    const date_to = req.body.date_to;
+    try {
+      const resp = await sequelize.query("call getDates(:date_f,:date_t)", {
+        replacements: {
+          date_f: date_from,
+          date_t: date_to,
+        },
+      });
 
       res.status(200).json({
         result: resp,
--- routes/excel.js
+++ routes/excel.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.post("/", excelController.index);
 router.post("/all", excelController.all);
 router.post("/ville", excelController.ville);
+router.post("/dates", excelController.dates);
 
 module.exports = router;
